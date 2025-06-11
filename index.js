306b91ee9a77505c7f63d4ee55130ff44eacca59
MarGit-1000 <ikramazzamy129@gmail.com>
2025-06-11 16:09:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/api', apiRouter);
 
 // Add CORS support
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
